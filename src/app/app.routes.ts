6173c8b2aaf8019fccf6b4162d4dde595cf48850
Nicolas Dos Santos Furlaneti <nicolas.furlanetii@gmail.com>
2026-09-08 21:04:34 -0300

    Versao 1.0 do formulario 08_09_2026

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ClienteCadastrar } from './cliente-cadastrar/cliente-cadastrar';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: ClienteCadastrar
+  }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ClienteCadastrar } from './cliente-cadastrar/cliente-cadastrar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ClienteCadastrar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
